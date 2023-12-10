a0bef69f18a1ccd67d53a366b9cf68361f0e1e82
Ledovskikh Viktoria <vl223@bk.ru>
2023-12-10 14:22:24 +0300

    скорректировала pom

--- src/main/java/ru.practicum/steps/Checks.java
+++ src/main/java/ru.practicum/steps/Checks.java
@@ -1,9 +1,11 @@
 package ru.practicum.steps;
 
+import io.qameta.allure.Step;
 import org.junit.Assert;
 
 public class Checks {
 
+    @Step("Проверяет, что полученное сообщение соответсвует ожидаемому")
     public static void checkResult(String expected, String messageError) {
         Assert.assertEquals("Ошибка!!! Ожидали сообщение --->" + expected + "<--Получили-->" + messageError, expected, messageError);
     }
